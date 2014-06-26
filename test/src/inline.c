e7aca41eaa055a64cf45a2fb732aaa4d1981a055
bright-tools <dev@brightsilence.com>
2014-06-26 23:02:46 +0100

    More tests

---
+++ test/src/inline.c
@@ -0,0 +1,12 @@
+inline void example( void )
+{
+}
+
+static inline void example2( void )
+{
+}
+
+int main( void )
+{
+    return 0;
+}
---
+++ test/src/var.c
@@ -0,0 +1,25 @@
+int x,z,p;
+int q;
+extern int t,l;
+static int j;
+
+void example( void )
+{
+    int x;
+    int z;
+    extern int q;
+    static int b;
+    register int o;
+}
+
+int main( void )
+{
+    int x;
+    int y;
+    int p;
+    extern int z;
+    extern int v;
+    auto int h;
+
+    return 0;
+}
