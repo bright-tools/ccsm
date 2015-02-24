713589c8f5bb77d85a5d0d1be3230a9dc028c15b
bright-tools <dev@brightsilence.com>
2015-02-24 23:44:40 +0000

    Fix up test .mk fragments & update fn_call_multiple_files based on latest changes

--- test/tests/fn_call_single_file.mk
+++ test/tests/fn_call_single_file.mk
@@ -1,9 +1,9 @@
-all: fn_call_single_file
+all: test_fn_call_single_file
 
 .PHONY: test_fn_call_single_file
-fn_call_single_file: STEP=test_fn_call_single_file
-fn_call_single_file: $(PASS)/out.log.fn_call_single_file $(PASS)/err.log.fn_call_single_file
-$(PASS)/out.log.fn_call_single_file $(PASS)/err.log.fn_call_single_file: src/fn_call.c $(TGT) 
+test_fn_call_single_file: STEP=test_fn_call_single_file
+test_fn_call_single_file: $(PASS)/out.log.test_fn_call_single_file $(PASS)/err.log.test_fn_call_single_file
+$(PASS)/out.log.test_fn_call_single_file $(PASS)/err.log.test_fn_call_single_file: src/fn_call.c $(TGT) 
 	@echo Test $(STEP): Function call count \(single file\)
 	$(TGT_E) $(EXCLUDE) src/fn_call.c --output-metrics=FUNC_CALLED_BY,LOCAL_FN_CALL_CNT,OP_FN_CALL* -- $(OUT)
 	$(CHECK_E)
--- test/tests/fn_count_multiple_files.mk
+++ test/tests/fn_count_multiple_files.mk
@@ -1,8 +1,8 @@
-all: fn_count_multiple_files
+all: test_fn_count_multiple_files
 
 .PHONY: test_fn_count_multiple_files
-fn_count_multiple_files: STEP=test_fn_count_multiple_files
-fn_count_multiple_files: $(PASS)/out.log.test_fn_count_multiple_files $(PASS)/err.log.test_fn_count_multiple_files
+test_fn_count_multiple_files: STEP=test_fn_count_multiple_files
+test_fn_count_multiple_files: $(PASS)/out.log.test_fn_count_multiple_files $(PASS)/err.log.test_fn_count_multiple_files
 $(PASS)/out.log.test_fn_count_multiple_files $(PASS)/err.log.test_fn_count_multiple_files: src/fn_call.c src/fn_call2.c $(TGT) 
 	@echo Test $(STEP): Function call count \(multiple file\)
 	$(TGT_E) $(EXCLUDE) src/fn_call.c src/fn_call2.c --output-metrics=FUNC_CNT -- $(OUT)
