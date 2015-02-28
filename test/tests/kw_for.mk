430deb37633f2951b329ec544ce4f8c80dcc6c2c
bright-tools <dev@brightsilence.com>
2015-02-28 12:10:04 +0000

    New tests

---
+++ test/tests/kw_for.mk
@@ -0,0 +1,9 @@
+all: test_kw_for
+
+.PHONY: test_kw_for
+test_kw_for: STEP=test_kw_for
+test_kw_for: $(PASS)/out.log.test_kw_for $(PASS)/err.log.test_kw_for
+$(PASS)/out.log.test_kw_for $(PASS)/err.log.test_kw_for: src/kw_for.c $(TGT) 
+	@echo Test $(STEP): Token/Statement Count: For
+	$(TGT_E) $(EXCLUDE) src/kw_for.c  --output-metrics=STMT_FOR_CNT,TOK_FOR -- $(OUT)
+	$(CHECK_E)
