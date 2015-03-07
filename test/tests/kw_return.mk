d7f49982ea2c0b99403137fe3e93ba2e8827119d
bright-tools <dev@brightsilence.com>
2015-03-07 13:09:59 +0000

    Correct name of test

--- test/tests/kw_return.mk
+++ test/tests/kw_return.mk
@@ -4,6 +4,6 @@ all: test_kw_return
 test_kw_return: STEP=test_kw_return
 test_kw_return: $(PASS)/out.log.test_kw_return $(PASS)/err.log.test_kw_return
 $(PASS)/out.log.test_kw_return $(PASS)/err.log.test_kw_return: src/kw_return.c src/kw_return2.c $(TGT) 
-	@echo Test $(STEP): Token/Statement Count: For
+	@echo Test $(STEP): Token/Statement Count: Return
 	$(TGT_E) $(EXCLUDE) src/kw_return.c src/kw_return2.c  --output-metrics=STMT_RETURN_CNT,TOK_RETURN -- $(OUT)
 	$(CHECK_E)
