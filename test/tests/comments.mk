860e90c765de88784b32da4f5d43707469102441
bright-tools <dev@brightsilence.com>
2015-03-07 17:25:18 +0000

    Rename HIS_STMT and HIS_COMF metrics

--- test/tests/comments.mk
+++ test/tests/comments.mk
@@ -5,5 +5,5 @@ test_comments: STEP=test_comments
 test_comments: $(PASS)/out.log.test_comments $(PASS)/err.log.test_comments
 $(PASS)/out.log.test_comments $(PASS)/err.log.test_comments: src/comment.c src/comment2.c $(TGT) 
 	@echo Test $(STEP): Comment Count
-	$(TGT_E) $(EXCLUDE) --exclude-file comment_excluded.h src/comment.c src/comment2.c  --output-metrics=COMMENT_CNT,COMMENT_BYTE_COUNT,HIS_COMF,HIS_STMT -- $(OUT)
+	$(TGT_E) $(EXCLUDE) --exclude-file comment_excluded.h src/comment.c src/comment2.c  --output-metrics=COMMENT_CNT,COMMENT_BYTE_COUNT,TOK_HIS_COMF,TOK_HIS_STMT -- $(OUT)
 	$(CHECK_E)
