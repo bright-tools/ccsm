1f2ae11dcddced56ed3611c2f337fa281063a6f8
bright-tools <dev@brightsilence.com>
2018-12-16 15:17:36 +0000

    Improve script to help build CCSM

--- utils/vagrant/buildit.sh
+++ utils/vagrant/buildit.sh
@@ -1,12 +1,62 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-cd llvm/tools
-svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
-echo "set(CCSM_DIR /ccsm)" >> clang/tools/CMakeLists.txt
-echo "add_subdirectory(\${CCSM_DIR}/SRC \${CMAKE_CURRENT_BINARY_DIR}/ccsm)" >> clang/tools/CMakeLists.txt
-cd ../..
-mkdir build
-cd build
-cmake -G "Unix Makefiles" ../llvm
-make -C tools/clang/tools/ccsm
+set -e
+
+if [ -z "$1" ]; then
+    echo "Please supply 1 parameter, the path to CCSM checkout (dir which contains 'src')"
+    exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+    echo "'$1' doesn't seem to be a directory"
+    exit 1
+fi
+
+if [[ ! -d "$1/src" ]]; then
+    echo "I expected to see CCSM's 'src' directory in $1, but didn't"
+    exit 1
+fi
+
+function basicToolCheck {
+    TOOLS="cmake svn grep"
+    for TOOL in $TOOLS; do
+        echo "Checking for $TOOL"
+        if [[ ! -x `which $TOOL` ]]; then
+            echo "Didn't find $TOOL"
+            exit 1
+        fi
+    done
+}
+
+basicToolCheck
+
+LLVM_DIR=llvm
+
+if [ -d $LLVM_DIR ]; then
+    svn up $LLVM_DIR
+else
+    svn co http://llvm.org/svn/llvm-project/llvm/trunk $LLVM_DIR
+fi
+
+CLANG_DIR=$LLVM_DIR/tools/clang
+
+if [ -d $CLANG_DIR ]; then
+    svn up $CLANG_DIR
+else
+    svn co http://llvm.org/svn/llvm-project/cfe/trunk $CLANG_DIR
+fi
+
+CMAKE_FILE=$CLANG_DIR/tools/CMakeLists.txt
+
+if [ $(grep -c CCSM_DIR $CMAKE_FILE) -lt 1 ]; then
+    echo "Adding CCSM to clang tools makefile"
+    echo "set(CCSM_DIR $1)" >> $CMAKE_FILE
+    echo "add_subdirectory(\${CCSM_DIR}/SRC \${CMAKE_CURRENT_BINARY_DIR}/ccsm)" >> $CMAKE_FILE
+else
+    echo "CCSM already seems to be in the clang tools makefile"
+fi
+
+cmake -G "Unix Makefiles" --build build llvm
+make -C build/tools/clang/tools/ccsm
+
+exit 0
